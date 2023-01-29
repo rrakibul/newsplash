f73ef07d1d79f038636ee13e1c988075eb39aa8e
rakibul <mriidb@gmail.com>
2023-01-29 20:36:53 +0600

    Try mixins and composables

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# AnnoPub
+# NewSplash
 
 This template should help get you started developing with Vue 3 in Vite.
 
---
+++ src/composables/useValidation.js
@@ -0,0 +1,64 @@
+export function useValidation() {
+  function validate(rules) {
+    // let rules = this.rules();
+    this.errors = [];
+
+    for (let key in rules) {
+      let ruleSet = rules[key];
+
+      ruleSet.split("|").forEach(
+        function (value) {
+          if (value === "required") {
+            if (!this.requiredRuleCheck(this.propValue(key))) {
+              this.errors[key] = key + " is required";
+            }
+          }
+
+          if (value.toLowerCase().includes("min:")) {
+            let parts = value.split(":");
+            if (!this.minRuleCheck(this.propValue(key), parts[1])) {
+              this.errors[key] = key + " minimum range: " + parts[1];
+            }
+          }
+
+          if (value.toLowerCase().includes("max:")) {
+            let parts = value.split(":");
+            if (!this.maxRuleCheck(this.propValue(key), parts[1])) {
+              this.errors[key] = key + " maximum range: " + parts[1];
+            }
+          }
+        }.bind(this)
+      );
+    }
+  }
+
+  function propValue(name) {
+    let parts = name.split(".");
+
+    //todo: make it dynamic
+    switch (parts.length) {
+      case 2:
+        return this.$data[parts[0]][parts[1]];
+      case 3:
+        return this.$data[parts[0]][parts[1]][parts[2]];
+      case 4:
+        return this.$data[parts[0]][parts[1]][parts[2]][parts[3]];
+      default:
+        return null;
+    }
+  }
+
+  function requiredRuleCheck(value) {
+    return value.length >= 1;
+  }
+
+  function minRuleCheck(value, limit) {
+    return value.length >= limit;
+  }
+
+  function maxRuleCheck(value, limit) {
+    return value.length <= limit;
+  }
+
+  return { validate }
+}
